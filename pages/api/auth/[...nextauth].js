ce794f74615dc13ae23177554f69d0187e28a813
amitleuva1987 <amitleuva1987@gmail.com>
2022-07-05 13:18:57 +0530

    Update - updated api

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,17 +6,17 @@ import EmailProvider from 'next-auth/providers/email'
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 export default NextAuth({
-//  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [
   //    OAuth authentication providers...
-    // EmailProvider({
-    //   server: process.env.EMAIL_SERVER,
-    //   from: process.env.EMAIL_FROM,
-    //   // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
-    // }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+      // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    }),
     // GoogleProvider({
     //   clientId: process.env.GOOGLE_ID,
     //   clientSecret: process.env.GOOGLE_SECRET
