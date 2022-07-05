38cbbae0a42acd55ac474122505847f290d2c23e
amitleuva1987 <amitleuva1987@gmail.com>
2022-07-05 12:13:49 +0530

    Update - Prisma client

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ import EmailProvider from 'next-auth/providers/email'
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export default NextAuth({
 //  adapter: PrismaAdapter(prisma),
