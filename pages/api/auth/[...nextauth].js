c8491f0ea32b696d3330c74d76b21328bd440675
amitleuva1987 <amitleuva1987@gmail.com>
2022-07-05 10:36:46 +0530

    Add - Added secreat key to the

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,13 +4,13 @@ import GithubProvider from "next-auth/providers/github"
 // import GoogleProvider from 'next-auth/providers/google'
 import EmailProvider from 'next-auth/providers/email'
 import nodemailer from "nodemailer"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+// import { PrismaAdapter } from "@next-auth/prisma-adapter"
+// import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export default NextAuth({
-  adapter: PrismaAdapter(prisma),
+//  adapter: PrismaAdapter(prisma),
   providers: [
     // OAuth authentication providers...
     // EmailProvider({
@@ -31,5 +31,6 @@ export default NextAuth({
     //   clientSecret: process.env.FACEBOOK_SECRET
     // }),
     // Passwordless / email sign in
-  ]
+  ],
+  secret: "LlKq6ZtYbr+hTC073mAmAh9/h2HwMfsFo4hrfCx5mLg=",
 })
