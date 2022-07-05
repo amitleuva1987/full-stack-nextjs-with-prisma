d4ca6eb01209a3a02f444163dd23f35f9621e86e
amitleuva1987 <amitleuva1987@gmail.com>
2022-07-05 09:59:06 +0530

    Update - Removed Emai Login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,11 +13,11 @@ export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     // OAuth authentication providers...
-    EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-      // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
-    }),
+    // EmailProvider({
+    //   server: process.env.EMAIL_SERVER,
+    //   from: process.env.EMAIL_FROM,
+    //   // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    // }),
     // GoogleProvider({
     //   clientId: process.env.GOOGLE_ID,
     //   clientSecret: process.env.GOOGLE_SECRET
