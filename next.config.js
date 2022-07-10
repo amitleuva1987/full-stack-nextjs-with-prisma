a5a2e1a8b33ecf9e9be8ef8b263a014463dc189c
amitleuva1987 <amitleuva1987@gmail.com>
2022-07-10 09:04:10 +0530

    Updated Next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+   env:{
+    NEXTAUTH_URL:'******',
+    GIT_ID:"*******",
+    GIT_SECRET:"*******",
+    EMAIL_SERVER:"*********",
+    EMAIL_FROM:"********",
+    DATABASE_URL:"********"
+   },
+   target: 'serverless',
 }
 
 module.exports = nextConfig
